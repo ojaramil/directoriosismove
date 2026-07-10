44a74f0d5c274f24d835723878271e5d1b1c1365
Orlando Jaramillo <orlando.jaramillo@gmail.com>
2026-07-10 18:13:47 -0500

    fix: usar nueva url de google apps script

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "directorio-sismo-v2";
+
+const CORE_ASSETS = [
+  "/",
+  "/index.html",
+  "/prioritario/",
+  "/prioritario/index.html",
+  "/mensajes/",
+  "/mensajes/index.html",
+  "/data.json",
+  "/portada-ayuda.jpg",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(CORE_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  if (req.method !== "GET" || !req.url.startsWith(self.location.origin)) return;
+
+  event.respondWith(
+    fetch(req)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(req, copy));
+        return res;
+      })
+      .catch(() => caches.match(req).then((cached) => cached || caches.match("/index.html")))
+  );
+});
